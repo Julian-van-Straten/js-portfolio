eeec9a0873670e13d9d3c4e4c244a24b934aa175
Julian van Straten <julian.v.straten@gmail.com>
2019-12-20 20:51:52 +0000

    basic projects wireframe

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.body.innerHTML = "<div id='container'></div>";
 const containerDiv = document.getElementById('container');
 
 const navBar = {
-    menuLinks : ["<a href='#' class='header-links'>home</a>", "<a href='#' class='header-links'>projects</a>", "<a href='#' class='header-links'>about</a>"]
+    menuLinks : ["<a href='#' class='header-links'>home</a>", "<a href='#' class='header-links'>projects</a>", "<a href='#' class='header-links'>about</a>", "<div id='theme-selector-container'><div id='switch'><div id='toggle'></div></div></div>"]
 }
 
 function generateSections(){
@@ -11,15 +11,19 @@ function generateSections(){
     "<section id='projects-section'></section>" +
     "<section id='about-section'></section>";
     
+    
 }
 
 function generateHeader(){
     const homeSection = document.getElementById('home-section');
     homeSection.innerHTML = "<header id='header'>" + 
     "<div id='logo'><h1>Julian van Straten</h1></div>" +
-    "<div id='theme-selector-container'><div id='switch'><div id='toggle'></div></div></div>"+
     "<div id='nav'></div>" +
-    "</header>";
+    "</header>"+
+    "<div id='hero-container'>"+
+    "<h1 id='hero-heading'>Hello, my name is Julian</h1>" +
+    "<p id='hero-paragraph'>I'm a front end developer<span id='cursor-blink'>_</span></p>"+
+    "</div>";
 }
 
 function generateMenu(){
@@ -40,16 +44,22 @@ function changeToLight(){
     const switchDiv = document.getElementById('switch');
     const headings = document.querySelector('h1');
     const links = document.getElementsByClassName('header-links');
+    
 
     for (let i = 0; i < links.length; i++){
-        links[i].style.color = "black";
+        links[i].style.color = "#6c567b";
     }
 
-    header.style.backgroundColor = 'white';
-    toggle.style.border = '2px solid black';
-    switchDiv.style.border = '2px solid black';
-    headings.style.color = 'black';
-    document.getElementById('home-section').style.backgroundColor = 'white'
+    header.style.backgroundColor = '#f8b195';
+    toggle.style.border = '2px solid #6c567b';
+    toggle.style.backgroundColor = '#6c567b';
+    switchDiv.style.border = '2px solid #6c567b';
+    headings.style.color = '#6c567b';
+    document.getElementById('home-section').style.backgroundColor = '#f8b195'
+    toggle.style.left = "20px";
+    document.getElementById('hero-heading').style.color = "#6c567b";
+    document.getElementById('hero-paragraph').style.color = "#6c567b";
+    document.getElementById('cursor-blink').style.color = "white";
     
     
 }
@@ -59,6 +69,7 @@ function changeToDark(){
     const toggle = document.getElementById('toggle');
     const switchDiv = document.getElementById('switch');
     const headings = document.querySelector('h1');
+    
 
     const links = document.getElementsByClassName('header-links');
 
@@ -69,29 +80,60 @@ function changeToDark(){
     header.style.backgroundColor = '';
     toggle.style.border = '';
     switchDiv.style.border = '';
+    toggle.style.backgroundColor = '';
     headings.style.color = '';
-    document.getElementById('home-section').style.backgroundColor = ''
+    document.getElementById('home-section').style.backgroundColor = '';
+    toggle.style.left = "0px";
+    document.getElementById('hero-heading').style.color = "";
+    document.getElementById('hero-paragraph').style.color = "";
+    document.getElementById('cursor-blink').style.color = "";
     
    
 }
 
+function generateRandomCircles(divTarget){
+    
+    let amountOfCircles = Math.floor((Math.random() * 500) + 10);
+
+    for (let i = 0; i < amountOfCircles; i++){
+        let randomNumber = Math.floor((Math.random() * 10) + 1);
+        let randomPositionX = Math.floor((Math.random() * 300) + -60);
+        let randomPositionY = Math.floor((Math.random() * 2000) + 10);
+        let randomOpacity = (Math.random() * 0.3).toFixed(1);
+        document.getElementById(divTarget).innerHTML += `<div class='circles' id='circle${i}'></div>`;
+        document.getElementById(`circle${i}`).style.cssText = "position: relative; z-index: 5;"+ `top: ${randomPositionX}px; left:${randomPositionY}px; opacity: ${randomOpacity};` +"width: "+ randomNumber + "px; height: " + randomNumber + "px; background-color: white; border-radius: 50%";
+    }
+    
+    
+}
+
+generateRandomCircles('home-section');
 
 
 
-
-
-
-
-
-const toggle = document.getElementById('toggle');
+let x = 0;
 
 toggle.onclick = () => {
-    if (toggle.style.left === ""){
-        toggle.style.left = "14px";
+const toggle = document.getElementById('toggle');
+
+
+    if (x === 0){
+        x++;
         changeToLight();
-    } else {
-        toggle.style.left = "";
+        
+       
+    } else{
+        x--;
         changeToDark();
+        
     }
     
 };
+
+function populateProjects(){
+    const projectsSection = document.getElementById('projects-section');
+    projectsSection.innerHTML = "<div class='page-titles'><h1>Projects</h1></div>"+
+    "<div id='projects-container'><div class='projects-boxes'></div><div class='projects-boxes'></div><div class='projects-boxes'></div></div>";
+}
+
+populateProjects();
